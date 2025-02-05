d63dca8914f3ee8a5d4c8d1d5f9e28cddd119642
Imad-Nofal <162768414+Imad-Nofal@users.noreply.github.com>
2025-02-05 12:04:55 +0200

    show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,19 @@
 <html>
-<head>
-    <title>Post Details</title>
-</head>
+<head><title>Post Details</title></head>
 <body>
-    @if(isset($post))
-        <h1>{{ $post->title }}</h1>
-        <p>{{ $post->description }}</p>
-        <p>Created At: {{ $post->created_at->diffForHumans() }}</p>
-    @else
-        <p>Post not found.</p>
-    @endif
+    <h1>{{ $post->title }}</h1>
+    <p>{{ $post->description }}</p>
+    <p>Created At: {{ $post->created_at->diffForHumans() }}</p>
+
+    @auth
+        <a href="{{ route('posts.edit', $post->id) }}">Edit</a>
+        <form method="POST" action="{{ route('posts.destroy', $post->id) }}" style="display:inline;">
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+        </form>
+    @endauth
+
+    <a href="{{ route('posts.index') }}">Back to Posts</a>
 </body>
 </html>
